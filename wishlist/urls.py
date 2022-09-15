c409de68db10030f0576d9333f2f486572e42553
eldiralps <eldiralps@gmail.com>
2022-09-15 15:43:38 +0700

    done tutorial 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_in_xml
+from wishlist.views import show_wishlist_in_json 
+from wishlist.views import show_data_json 
+from wishlist.views import show_data_xml
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
-
+    path('xml/', show_wishlist_in_xml, name='show_wishlist_in_xml'),
+    path('json/', show_wishlist_in_json, name='show_wishlist_in_json'),
+    path('json/<int:id>', show_data_json, name='show_data_json'), 
+    path('xml/<int:id>', show_data_xml, name='show_data_xml'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html", context)
-
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Eldira Lahanny P S'
-}
+    }
+    return render(request, "wishlist.html", context)
+
+def show_wishlist_in_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_wishlist_in_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_data_json(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_data_xml(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
